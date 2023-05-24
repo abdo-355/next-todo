7bcf43180b1d6455fcda9eaec833e103c1f33f44
abdo-355 <abdoluxor366@gmail.com>
2023-05-25 00:47:06 +0300

    clean the app and add the 'route' and the todoItem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,12 @@
+interface Props {
+    id: string;
+    title: string;
+    complete: boolean;
+}
+
+export default function TodoItem({ id, title, complete }: Props) {
+    return <li className="flex gap-1 items-center">
+        <input id={id} type="checkbox" className="cursor-pointer peer" />
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+    </li>
+}
