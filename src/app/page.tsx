db0c8c812a9c311d4b8edb903065a9522becaa6d
abdo-355 <abdoluxor366@gmail.com>
2023-05-25 01:33:06 +0300

    update the tode item complete status in the database with the help of server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,16 @@ import Link from "next/link";
 import { prisma } from "@/db";
 import TodoItem from "@/components/TodoItem";
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({
+    where: { id },
+    data: { complete: complete }
+  })
+}
+
+
 export default async function Home() {
   const todos = await prisma.todo.findMany()
 
@@ -13,7 +23,7 @@ export default async function Home() {
     </header>
     <ul className="pl-4">
       {todos.map(todo =>
-        <TodoItem key={todo.id} {...todo} />
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
       )}
     </ul>
   </>
